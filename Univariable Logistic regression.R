d67877cfcce95e8b9f6ee6a483be6fcea3811efc
statrad <45779343+statrad@users.noreply.github.com>
2018-12-27 14:13:42 +0900

    Update Univariable Logistic regression.R

--- Univariable Logistic regression.R
+++ Univariable Logistic regression.R
@@ -30,46 +30,84 @@
 #    -y : response variable 
 #    -filename : "filename", in case of specfied working directory  or full path for the file.
 
-unilog<-function(dat,pnum,y,filename="result"){
+#########################NOTE##########################
+
+# There are two errors you may got while you use.
+# 1.  Error: Can't use matrix or array for column indexing  
+#     - It may occurs if you put the data as a "tibble" type or something. Please convert it as a data.frame.
+# 2.  Error in summary(a)[, 4] : incorrect number of dimensions
+#     - This is more typical error. It returns this message when you give it wrong parameter(variable name).
+#     - Please confirm your column names by command "colnames(data)" and input right column name.
+
+######################################################
+
+### Example codes will be offered.###
+
+unilog<-function(dat,var.list,y,filename="result"){
   library(doBy)
   library(logistf)
   
   rownum<-0
-  for(i in 1 : ncol(dat)){
-    if(identical(dat[,i],y)|identical(dat[,i],pnum)){next}
+  index<-c()
+  y.name<-y
+  y<-dat[,grep(y,colnames(data))]
+  
+  if(length(var.list)==1){
+    index <- seq(grep(var.list,colnames(dat)),ncol(dat))
+  }else if(length(var.list)==2){
+    index<- seq(grep(var.list[1],colnames(dat)),grep(var.list[2],colnames(dat)))
+  }else{
+    for(i in 1:length(var.list)){
+      index<-c(index,grep(var.list[i],colnames(dat)))
+    }
+  }
+  for(i in index){
+    if(colnames(dat)[i]==y.name){next}
     if(is.factor(dat[,i])){
       rownum<-rownum+length(levels(dat[,i]))-1
     }else{
       rownum<-rownum+1
     }
   }
+  
   table <- matrix(NA,ncol = 4,nrow= rownum)
   colnames(table)<-c("variable","odds ratio","p-value","remarks")
   j<-1
-
-  for(i in 1:ncol(dat)){ 
-      
-    if(identical(dat[,i],y)|identical(dat[,i],pnum)){
-       next
-    }
+  
+  for(i in index){ 
+    if(colnames(dat)[i]==y.name){next}
     
-    table[j,"variable"]<-names(dat)[i]
+    
     t.temp<-table(dat[,i],y)
     
     if(sum(t.temp==0)>=1&length(levels(as.factor(dat[,i])))<=5){
       fit<-logistf(y~dat[,i])
-      table[j,"p-value"]<-round(fit$prob[2],3)
-      table[j,"odds ratio"]<-paste(round(exp(coef(fit))[2],3)," (",round(exp(confint(fit))[2,1],3),", ",round(exp(confint(fit))[2,2],3),")")
-      table[j,"remarks"]<-"firth logistic"
+ 
+      for(k in 2 : length(fit$coefficients)){
+         table[j,"variable"]<-paste(names(dat)[i]," ",levels(dat[,i])[k])
+         table[j,"p-value"]<-round(fit$prob[k],3)
+         table[j,"odds ratio"]<-paste(round(exp(coef(fit))[k],3)," (",round(exp(confint(fit))[k,1],3),", ",round(exp(confint(fit))[k,2],3),")")
+         table[j,"remarks"]<-"firth logistic"
+         j<-j+1
+      }   
+      
     }else{
       fit<-glm(y~dat[,i],family = binomial)
-      table[j,"p-value"]<-round(coef(summary(fit))[,4][2],3)
-      table[j,"odds ratio"]<-paste(round(exp(coef(fit))[2],3)," (",round(exp(confint(fit))[2,1],3),", ",round(exp(confint(fit))[2,2],3),")")      
+      
+       for(k in 2 : length(fit$coefficients)){
+         table[j,"variable"]<-paste(names(dat)[i]," ",levels(dat[,i])[k])
+        table[j,"p-value"]<-round(coef(summary(fit))[,4][k],3)
+        table[j,"odds ratio"]<-paste(round(exp(coef(fit))[k],3)," (",round(exp(confint(fit))[k,1],3),", ",round(exp(confint(fit))[k,2],3),")")
+        j<-j+1
+      }   
+      
     }
-     
-    j<-j+1
+    
     
   }
   write.csv(table,paste(filename,".csv",sep=""),row.names=F)
   return(table)
 }
+
+
+
